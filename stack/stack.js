205292ccdad3da5c30ab7c8cadd18ccba48e2804
devang <dev.paliwal@gmail.com>
2013-03-07 00:20:45 +0530

    corrected stack impl

--- stack/stack.js
+++ stack/stack.js
@@ -1,42 +1,15 @@
-/*
-	Stack Implementation
-*/
-function Stack(){
-
-	if(this.initialized){
-
-		return this.instance;
+var Stack = function(){
+	var stack = [];
+	return {
+		push:function(obj){
+			stack.push(obj)
+		},
+		pop:function(){
+			if(stack.length == 0) throw Error("Stack is empty");
+			return stack.pop();
+		},
+		size:function(){
+			return stack.length;
+		}
 	}
-
-	var push = function(){
-
-	}
-
-	var pop = function(){
-
-	}
-
-	var size = function(){
-
-	}
-
-	var top = function(){
-
-	}
-
-	var isEmpty = function(){
-
-	}
-
-
-	this.instance = {
-		'push':push,
-		'pop':pop,
-		'size':size,
-		'isEmpty':isEmpty,
-		'top':top
-	}
-
-	return this.instance;
-
 }
