15ce3ad7e142f7337c1e015112802bf86b1b9ee1
devang <dev.paliwal@gmail.com>
2013-03-06 23:58:35 +0530

    stack

--- stack/stack.js
+++ stack/stack.js
@@ -1,4 +1,42 @@
 /*
 	Stack Implementation
-
 */
+function Stack(){
+
+	if(this.initialized){
+
+		return this.instance;
+	}
+
+	var push = function(){
+
+	}
+
+	var pop = function(){
+
+	}
+
+	var size = function(){
+
+	}
+
+	var top = function(){
+
+	}
+
+	var isEmpty = function(){
+
+	}
+
+
+	this.instance = {
+		'push':push,
+		'pop':pop,
+		'size':size,
+		'isEmpty':isEmpty,
+		'top':top
+	}
+
+	return this.instance;
+
+}
